ef3ca30158a5c4ab64596adcd4f2982543cc7c60
Viktor Polyakov <polyakov.viktor8@wb.ru>
2025-01-09 23:54:10 +0300

    feat: add linters

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,36 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require('lspconfig')
-    -- Lua
-    lspconfig.lua_ls.setup({
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      settings = {
-        Lua = {
-            diagnostics = {
-               -- Get the language server to recognize the `vim` global
-                globals = { 'vim' }
-            }
-        }
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-   })
-   -- Go
-   lspconfig.gopls.setup({})
-  end
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
+		-- Lua
+		lspconfig.lua_ls.setup({
+			runtime = {
+				-- Tell the language server which version of Lua you're using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			settings = {
+				Lua = {
+					diagnostics = {
+						-- Get the language server to recognize the `vim` global
+						globals = { "vim" },
+					},
+				},
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		})
+		-- Go
+		lspconfig.gopls.setup({})
+		lspconfig.golangci_lint_ls.setup({})
+		lspconfig.buf_ls.setup({})
+		-- SQL
+		lspconfig.sqlls.setup({})
+	end,
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,16 +1,22 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require('mason').setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls", "gopls" },
-      })
-    end
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"golangci_lint_ls",
+					"buf_ls",
+					"sqlls",
+				},
+			})
+		end,
+	},
 }
